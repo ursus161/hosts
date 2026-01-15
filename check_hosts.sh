0d7bdd3605194620d9c295b96a8c91569f5f425a
Stefan Ghetu <stefan.ghetu9@gmail.com>
2026-01-15 16:15:56 +0200

    Added IP validation script

--- check_hosts.sh
+++ check_hosts.sh
@@ -3,9 +3,16 @@ cat /etc/hosts | while read line
 do
     ip=$(echo $line | awk '{print $1}')
     nume=$(echo $line | awk '{print $2}')
-    adresa=$(nslookup $nume 2>/dev/null | grep "Address:" | tail -1 | awk '{print $2}')
     
-    if [[ "$ip" != "$adresa" ]]; then
+    if [[ -z "$line" ]] || [[ "$line" == "#"* ]]; then
+        continue
+    fi 
+    #linii goale sau comentarii, se trece peste (evident tot ce incepe cu # e comentariu)
+    adresa=$(nslookup $nume 8.8.8.8 2>/dev/null | grep "Address:" | tail -1 | awk '{print $2}')
+    #modificarea este pasarea unui server DNS ca argument, puteam sa pun si 1.1.1.1 pentru un req la DNS-ul Cloudflare
+    #by default, cum gresisem initial mi se ducea in /etc/hosts si imi gasea gresit din cauza modificarii mele
+    #doar daca nu gasea se duce sa faca request la se ducea la DNS-ul implicit (/etc/resolv.conf), insa acum se duce mai intai la 8.8.8.8 si ignora /etc/hosts 
+    if [[ -n "$adresa" ]] && [[ "$ip" != "$adresa" ]]; then
         echo "Bogus IP for $nume in /etc/hosts!"
     fi
 done
